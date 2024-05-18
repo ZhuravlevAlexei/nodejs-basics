bb22220d64871fc69fa39df166b075b1d33c51ff
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-05-18 17:00:20 +0300

    up to http-errors

--- src/server.js
+++ src/server.js
@@ -4,18 +4,13 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { getAllStudents, getStudentById } from './services/students.js';
-
-// Читаємо змінну оточення PORT правильно функцією з файлу
+import studentsRouter from './routers/students.js';
 import { env } from './utils/env.js';
+// Імпортуємо middleware
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 const PORT = Number(env('PORT', '3000'));
-// спочатку просто
-// const PORT = 3000;
-//а можна так
-// import dotenv from 'dotenv';
-// dotenv.config();
-// Читаємо змінну оточення PORT
-// const PORT = Number(process.env.PORT);
 
 export const startServer = () => {
   const app = express();
@@ -33,35 +28,15 @@ export const startServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello world!',
+      message: 'Hello World!',
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(studentsRouter);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    console.log('studentId: ', studentId);
-    const student = await getStudentById(studentId);
-    console.log('student: ', student);
-
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
