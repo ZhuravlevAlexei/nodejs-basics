da2ae2b000c926eea98af150462fa9ce3417cbc0
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-06-14 11:02:21 +0300

    before slugger

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,8 @@
 // src/services/auth.js
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 import { randomBytes } from 'crypto';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
@@ -14,9 +18,34 @@ import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 import { sendEmail } from '../utils/sendMail.js';
 
-import handlebars from 'handlebars';
-import path from 'node:path';
-import fs from 'node:fs/promises';
+import {
+  getFullNameFromGoogleTokenPayload,
+  validateCode,
+} from '../utils/googleOAuth2.js';
+
+export const loginOrSignupWithGoogle = async (code) => {
+  const loginTicket = await validateCode(code);
+  const payload = loginTicket.getPayload();
+  if (!payload) throw createHttpError(401);
+
+  let user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    const password = await bcrypt.hash(randomBytes(10), 10);
+    user = await UsersCollection.create({
+      email: payload.email,
+      name: getFullNameFromGoogleTokenPayload(payload),
+      password,
+      role: 'parent',
+    });
+  }
+
+  const newSession = createSession();
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    ...newSession,
+  });
+};
 
 export const resetPassword = async (payload) => {
   let entries;
