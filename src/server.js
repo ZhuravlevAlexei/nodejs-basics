c783f4ae208332f721bfdbb24c5bbe28adbe1e54
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-06-15 08:03:03 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -14,12 +14,16 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 import { UPLOAD_DIR } from './constants/index.js';
+
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
 
   app.use('/uploads', express.static(UPLOAD_DIR)); // устанавка маршрута для обробки файлів
+  app.use('/api-docs', swaggerDocs());
 
   // app.use(express.json());
   app.use(
